0a70fd652a7f28d984445faa7dc09dc2da257733
Inarat <hussaininarat03@gmail.com>
2026-06-03 23:55:54 +0530

    Added bg

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,34 @@ import Skills from './components/Skills';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 import Certifications from './components/Certifications';
+import PixelSnow from './components/PixelSnow';
 
 export default function App() {
   return (
     <>
+      <div style={{
+          position: 'fixed',
+          top: 0,
+          left: 0,
+          width: '100vw',
+          height: '100vh',
+          zIndex: -1,
+        }}>
+        <PixelSnow 
+          color="#00cc6a"
+          flakeSize={0.01}
+          minFlakeSize={1.25}
+          pixelResolution={200}
+          speed={1.25}
+          density={0.3}
+          direction={125}
+          brightness={1}
+          depthFade={8}
+          farPlane={20}
+          gamma={0.4545}
+          variant="square"
+      />
+    </div>
       <Navbar />
       <main>
         <Hero />
